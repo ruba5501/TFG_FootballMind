ff15aed76953a46de6af19057a27accc435ee6d8
ruba5501 <rubenescalonilla@gmail.com>
2025-09-14 14:07:55 +0200

    cambio en model de club (falta cambiar el json)

--- backend/models/club.js
+++ backend/models/club.js
@@ -3,32 +3,34 @@ const mongoose = require('mongoose');
 const clubSchema = new mongoose.Schema({
   nombre: { type: String, required: true, unique: true },
   ciudad: { type: String, required: true },
-  estadio: { type: String, required: true },
+  estadio: { type: mongoose.Schema.Types.ObjectId, ref: 'Estadio' },
 
-  presupuesto: {
-    traspasos: { type: Number, default: 0 },
-    salarios: { type: Number, default: 0 }
-  },
-
+  presupuestoTraspasos: { type: Number, default: 0 },
+  presupuestoSalarios: { type: Number, default: 0 },
   ingresos: {
     entradas: { type: Number, default: 0 },
     television: { type: Number, default: 0 },
     merchandising: { type: Number, default: 0 }
   },
 
-  jugadores: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Jugador' }],
-  empleados: [{ type: String }], //TO DO hacer un modelo Empleado
-
-  popularidad: { type: Number, min: 0, max: 100, default: 50 },
-  reputacion: { type: Number, min: 0, max: 100, default: 50 },
+  plantilla: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Jugador' }],
+  empleados: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Empleado' }],
 
   infraestructuras: {
-    entrenamiento: { type: Number, min: 1, max: 5, default: 3 },
-    cantera: { type: Number, min: 1, max: 5, default: 3 }
+    entrenamiento: { type: Number, min: 1, max: 5, default: 1 },
+    cantera: { type: Number, min: 1, max: 5, default: 1 }
   },
 
-  //escudo: { type: String }, // URL a la imagen
-  historialTitulos: [{ type: String }]
+  escudo: { type: String, default: null }, // URL
+  popularidad: { type: Number, min: 0, max: 100, default: 0 },
+  reputacion: { type: Number, min: 0, max: 100, default: 0 },
+  historialTitulos: [
+    {
+      competicion: { type: String, required: true },
+      cantidad: { type: Number, default: 0 },
+      trofeo: { type: String, default: null } // URL
+    }
+  ]
 });
 
 module.exports = mongoose.model('Club', clubSchema);
